ce76225d457d3daa595b18e824512e42e65113df
Ankit <ankit10093@gmail.com>
2022-03-27 16:45:42 +0530

    starting assingment 2

---
+++ Assignments_AnkitSoni/2_Tomcat/README.md
@@ -0,0 +1,4 @@
+# 2_Tomcat
+
+TODO: Enter the cookbook description here.
+
---
+++ Assignments_AnkitSoni/2_Tomcat/metadata.rb
@@ -0,0 +1,15 @@
+name '2_Tomcat'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures 2_Tomcat'
+long_description 'Installs/Configures 2_Tomcat'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/2_Tomcat/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/2_Tomcat' if respond_to?(:source_url)
