5779734834f6e9cfbd54bc21a601bea866a9bdaf
Ankit <ankit10093@gmail.com>
2022-03-27 15:46:45 +0530

    MongoDB task recipe file completed

--- Assignments_AnkitSoni/1_MongoDB/recipes/default.rb
+++ Assignments_AnkitSoni/1_MongoDB/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) 2022 The Authors, All Rights Reserved.
+
+
+template '/etc/yum.repos.d/mongodb-org-4.4.repo' do
+    source 'mongoDBContent.txt'
+    owner 'root'
+    mode 0644
+  end
+  
+  package 'mongodb-org'
+  
+  service 'mongod' do
+    action [:enable, :start]
+  end
+  
+  execute 'mongodb' do
+    command 'sudo chkconfig mongod on'
+  end
+  
+  
