229014a29f9c1404d6ba5fe94815ed67b9ce499b
Ankit <ankit10093@gmail.com>
2022-03-27 15:08:03 +0530

    Created MongoDB project

---
+++ Assignments_AnkitSoni/1_MongoDB/metadata.rb
@@ -0,0 +1,15 @@
+name '1_MongoDB'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures 1_MongoDB'
+long_description 'Installs/Configures 1_MongoDB'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/1_MongoDB/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/1_MongoDB' if respond_to?(:source_url)
---
+++ Assignments_AnkitSoni/1_MongoDB/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: 1_MongoDB
+# Recipe:: default
+#
+# Copyright (c) 2022 The Authors, All Rights Reserved.
